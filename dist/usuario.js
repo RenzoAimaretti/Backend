36088dbbb46d19fd860a718902e4422feb982fb2
Renzo <renzoceronueve@gmail.com>
2024-05-10 16:25:11 -0300

    first commit

---
+++ dist/usuario.js
@@ -0,0 +1,20 @@
+export class Usuario {
+    constructor(nombre, apellido, edad, email, password, genero, telefono, direccion, ciudad, pais, codigoPostal, fechaNacimiento, estado, rol, id) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.email = email;
+        this.password = password;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.pais = pais;
+        this.codigoPostal = codigoPostal;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+        this.rol = rol;
+        this.id = id;
+    }
+}
+//# sourceMappingURL=usuario.js.map
