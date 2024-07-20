7e09bbcc196a316587bb287fe5add82a3140674a
juanmanueldeelia <juanmadeelia@gmail.com>
2024-07-20 18:20:56 -0300

    modificacion app.js

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,8 @@ import { listRouter } from "./list/list.routes.js";
 import 'reflect-metadata';
 import { orm, syncSchema } from "./shared/db/orm.js";
 import { RequestContext } from "@mikro-orm/core";
+import {subscriptionRouter} from "./subscription/subscription.routes.js";
+   
 const app = express();
 app.use(express.json());
 //adicion del middleware orm despues de los base y antes de los de negocio y sus rutas
@@ -13,6 +15,7 @@ app.use((req, res, next) => {
 //Rutas
 app.use('/api/users', userRouter);
 app.use('/api/lists', listRouter);
+app.use('/api/subscription',subscriptionRouter) // /api/users/subscription?
 await syncSchema(); // solo en dev, NO SE DEBE USAR EN PRODUCCION
 app.listen(3000, () => {
     console.log('Server is running on port 3000 http://localhost:3000/');
