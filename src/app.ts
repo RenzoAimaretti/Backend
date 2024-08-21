d24b1037a214aa83746cd36e6b1b1561af9ddd38
Renzo Aimaretti <renzoceronueve@gmail.com>
2024-08-21 19:24:23 -0300

    add dashboard componente y persistencia mediante cookies

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import { authRouter } from "./shared/session/auth.routes.js";
 import cors from 'cors';
 import cookieParser from 'cookie-parser'
 import jwt from "jsonwebtoken";
+import { dashboardRouter } from "./shared/dashboard.routes.js";
 
 const app=express()
 app.use(express.json())
@@ -51,6 +52,10 @@ app.use('/api/lists', listRouter)
 app.use('/api/rangos', rangoRouter)
 app.use('/api/subscription',subscriptionRouter) // /api/users/subscription?
 app.use('/api/auth', authRouter)
+app.use('/api/dashboard',dashboardRouter)
+
+
+
 await syncSchema() // solo en dev, NO SE DEBE USAR EN PRODUCCION
 
 app.listen(3000,()=>{
--- src/shared/session/auth.controller.ts
+++ src/shared/session/auth.controller.ts
@@ -14,7 +14,6 @@ async function register(req: Request, res: Response) {
 
     // Hashear la contraseña
     const password= await bcrypt.hash(req.body.password, 10);
-    console.log("paso por aca")
     const user = em.create(User,{
         name: name,
         email:email,
@@ -54,9 +53,10 @@ async function login(req: Request, res: Response) {
 
         res.cookie('access_token', token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'strict',
-            maxAge:1000*60*60
+            secure: false,
+            sameSite: 'none',
+            maxAge:1000*60*60,
+            path:'/'
         }).send({message:'Successful login',data:userWithoutPassword,token});
 
     } else {
@@ -65,12 +65,11 @@ async function login(req: Request, res: Response) {
 }
 
 function logout(req: Request, res: Response) {
-    req.session.destroy(err => {
-        if (err) {
-            return res.status(500).json({ message: 'Logout failed' });
-        }
-        res.status(200).json({ message: 'Logout successful' }).clearCookie('access_token');
-    });
+    res.clearCookie('access_token', {
+    path: '/',
+    sameSite: 'none', 
+    secure: false
+});
 }
 
 export { register, login, logout };
