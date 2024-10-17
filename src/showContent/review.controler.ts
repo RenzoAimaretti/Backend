43ca57e872af2d12361520c669261d7fe8c9af05
Renzo Aimaretti <renzoceronueve@gmail.com>
2024-10-17 15:50:40 -0300

    Fix delete review with comments
    se agrego el populate de comments a la hora de eliminar una review para que se borren los comments asociados a una review

--- src/showContent/review.controler.ts
+++ src/showContent/review.controler.ts
@@ -1,79 +1,99 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { User } from "../user/user.entity.js";
 import { orm } from "../shared/db/orm.js";
 import { ShowContent } from "./showContent.entity.js";
 import { Review } from "./review.entity.js";
-import { addOneContent,findOneContent } from "./showContent.controler.js";
+import { addOneContent, findOneContent } from "./showContent.controler.js";
 
-const em = orm.em
+const em = orm.em;
 
-async function addOneReview(req:Request,res:Response){
-    try{
-        const content = await findOneContent(req,res)
-        const id = Number.parseInt(req.params.id)
-        const user=await em.findOneOrFail(User, {id}, {populate:['rangoCinefilo','friends','friendsFrom','lists','followingLists','subscription']})
-        
-        if(content!=null){
-            const newReview= em.create(Review,{
-                rating:req.body.rating,
-                description:req.body.description,
-                reviewOwner:user.id,
-                showReviewd:content,
-                comments:[],
-            });
-            await em.persistAndFlush(newReview)
-        }else{
-            addOneContent(req,res)
-            const content = await findOneContent(req,res) as ShowContent
-            const newReview= em.create(Review,{
-                rating:req.body.rating,
-                description:req.body.description,
-                reviewOwner:user.id,
-                showReviewd:content,
-                comments:[],
-            });
-            await em.persistAndFlush(newReview)
-            
-        }
-        res.status(200).json({message:'Review created'})
-    }catch(error:any){
-        res.status(500).json({message:error.message})
-    }   
-}
+async function addOneReview(req: Request, res: Response) {
+  try {
+    const content = await findOneContent(req, res);
+    const id = Number.parseInt(req.params.id);
+    const user = await em.findOneOrFail(
+      User,
+      { id },
+      {
+        populate: [
+          "rangoCinefilo",
+          "friends",
+          "friendsFrom",
+          "lists",
+          "followingLists",
+          "subscription",
+        ],
+      }
+    );
 
-async function deleteOneReview(req:Request, res:Response){
-    try{
-        const reviewOwner= Number.parseInt(req.params.id);
-        const showReviewd = await findOneContent(req,res) as ShowContent
-        const review= await em.findOneOrFail(Review, {reviewOwner, showReviewd});
-        em.removeAndFlush(review);
-        res.status(200).json({message:'review deleted',data:review})
-    }catch(error:any){
-        res.status(500).json({message:error.message})
+    if (content != null) {
+      const newReview = em.create(Review, {
+        rating: req.body.rating,
+        description: req.body.description,
+        reviewOwner: user.id,
+        showReviewd: content,
+        comments: [],
+      });
+      await em.persistAndFlush(newReview);
+    } else {
+      addOneContent(req, res);
+      const content = (await findOneContent(req, res)) as ShowContent;
+      const newReview = em.create(Review, {
+        rating: req.body.rating,
+        description: req.body.description,
+        reviewOwner: user.id,
+        showReviewd: content,
+        comments: [],
+      });
+      await em.persistAndFlush(newReview);
     }
+    res.status(200).json({ message: "Review created" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-async function getContentReviews(req:Request,res:Response){
-    try{
-        const content = await findOneContent(req,res)
-        const reviews = await em.find(Review,{showReviewd:content},{populate:['reviewOwner','comments.commentReview.reviewOwner']})
-        res.status(200).json({message:'Reviews found',data:reviews})
-    }catch(error:any){
-        res.status(500).json({message:error.message})
-    }
+async function deleteOneReview(req: Request, res: Response) {
+  try {
+    const reviewOwner = Number.parseInt(req.params.id);
+    const showReviewd = (await findOneContent(req, res)) as ShowContent;
+    const review = await em.findOneOrFail(
+      Review,
+      { reviewOwner, showReviewd },
+      { populate: ["comments"] }
+    );
+    em.removeAndFlush(review);
+    res.status(200).json({ message: "review deleted", data: review });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-async function editReview(req:Request,res:Response){
-    try{
-        const reviewOwner= Number.parseInt(req.params.id);
-        const showReviewd = await findOneContent(req,res) as ShowContent
-        const review= await em.findOneOrFail(Review, {reviewOwner, showReviewd});
-        review.rating = req.body.rating;
-        review.description = req.body.description;
-        await em.flush();
-        res.status(200).json({message:'review edited',data:review})
-    }catch(error:any){
-        res.status(500).json({message:error.message})
-    }
+async function getContentReviews(req: Request, res: Response) {
+  try {
+    const content = await findOneContent(req, res);
+    const reviews = await em.find(
+      Review,
+      { showReviewd: content },
+      { populate: ["reviewOwner", "comments.commentReview.reviewOwner"] }
+    );
+    res.status(200).json({ message: "Reviews found", data: reviews });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
-export { addOneReview, deleteOneReview, getContentReviews, editReview}
+
+async function editReview(req: Request, res: Response) {
+  try {
+    const reviewOwner = Number.parseInt(req.params.id);
+    const showReviewd = (await findOneContent(req, res)) as ShowContent;
+    const review = await em.findOneOrFail(Review, { reviewOwner, showReviewd });
+    review.rating = req.body.rating;
+    review.description = req.body.description;
+    await em.flush();
+    res.status(200).json({ message: "review edited", data: review });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+}
+export { addOneReview, deleteOneReview, getContentReviews, editReview };
--- src/showContent/review.entity.ts
+++ src/showContent/review.entity.ts
@@ -1,22 +1,35 @@
-import { Entity,Cascade, Property, ManyToOne, PrimaryKeyType, PrimaryKeyProp, ManyToMany, OneToMany, Collection } from "@mikro-orm/core";
+import {
+  Entity,
+  Cascade,
+  Property,
+  ManyToOne,
+  PrimaryKeyType,
+  PrimaryKeyProp,
+  ManyToMany,
+  OneToMany,
+  Collection,
+} from "@mikro-orm/core";
 import { User } from "../user/user.entity.js";
 import { ShowContent } from "./showContent.entity.js";
 import { Comment } from "./comment.entity.js";
 
 @Entity()
 export class Review {
-    @Property({nullable:false})
-    rating!:number;
+  @Property({ nullable: false })
+  rating!: number;
 
-    @Property({nullable:true})
-    description!:string;
+  @Property({ nullable: true })
+  description!: string;
 
-    @ManyToOne(()=>User,{nullable:false, primary:true})
-    reviewOwner!:User;
+  @ManyToOne(() => User, { nullable: false, primary: true })
+  reviewOwner!: User;
 
-    @ManyToOne(()=>ShowContent,{nullable:false, primary:true})
-    showReviewd!:ShowContent;
+  @ManyToOne(() => ShowContent, { nullable: false, primary: true })
+  showReviewd!: ShowContent;
 
-    @OneToMany(()=>Comment, comment=>comment.commentReview, {cascade:[Cascade.ALL],nullable:true})
-    comments =  new Collection<Comment>(this);
+  @OneToMany(() => Comment, (comment) => comment.commentReview, {
+    cascade: [Cascade.ALL],
+    nullable: true,
+  })
+  comments = new Collection<Comment>(this);
 }
