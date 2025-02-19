669c1f58aa9aa174fb232a949f8f5f8889f80f9e
juanmanueldeelia <141456353+juanmanueldeelia@users.noreply.github.com>
2025-02-19 19:36:35 -0300

    Update review.controller.ts

--- src/showContent/review.controller.ts
+++ src/showContent/review.controller.ts
@@ -4,6 +4,7 @@ import { orm } from "../shared/db/orm.js";
 import { ShowContent } from "./showContent.entity.js";
 import { Review } from "./review.entity.js";
 import { addOneContent, findOneContent } from "./showContent.controller.js";
+import { perspectiveAnalisys } from "../shared/perspective.controller.js";
 import { RangoCinefilo } from "../rangoCinefilo/rangoCinefilo.entity.js";
 const em = orm.em;
 
@@ -11,53 +12,46 @@ async function addOneReview(req: Request, res: Response) {
   try {
     const content = await findOneContent(req, res);
     const id = Number.parseInt(req.params.id);
-    const user = await em.findOneOrFail(
-      User,
-      { id },
-      {
-        populate: [
-          "rangoCinefilo",
-          "friends",
-          "friendsFrom",
-          "lists",
-          "followingLists",
-          "subscription",
-        ],
-      }
-    );
-
-    if (content != null) {
-      const newReview = em.create(Review, {
-        rating: req.body.rating,
-        description: req.body.description,
-        reviewOwner: user.id,
-        showReviewd: content,
-        comments: [],
-      });
-      await em.persistAndFlush(newReview);
-    } else {
-      addOneContent(req, res);
-      const content = (await findOneContent(req, res)) as ShowContent;
-      const newReview = em.create(Review, {
-        rating: req.body.rating,
-        description: req.body.description,
-        reviewOwner: user.id,
-        showReviewd: content,
-        comments: [],
-      });
-      await em.persistAndFlush(newReview);
-    }
-       
-    const reviewsCount = await em.count(Review, { reviewOwner: user.id });
-
-    
-    const newRangoCinefilo = await em.findOne(RangoCinefilo, {
-      minReviews: { $lte: reviewsCount },
-    }, {
-      orderBy: { minReviews: 'DESC' }, 
+    const user = await em.findOneOrFail(User, { id }, {
+      populate: ["rangoCinefilo", "friends", "friendsFrom", "lists", "followingLists", "subscription"],
     });
 
-    
+    const toxicityScore = await perspectiveAnalisys.analyzeText(req.body.description);
+    if (toxicityScore == null) {
+      return res.status(500).json({ message: "Error with Perspective API" });
+    }
+    if (toxicityScore > 0.7) {
+      return res.status(200).json({ message: "Toxicity detected" });
+    }
+
+    let newReview: Review;
+    if (content) {
+      newReview = em.create(Review, {
+        rating: req.body.rating,
+        description: req.body.description,
+        reviewOwner: user.id,
+        showReviewd: content,
+        comments: [],
+      });
+    } else {
+      await addOneContent(req, res);
+      const newContent = (await findOneContent(req, res)) as ShowContent;
+      newReview = em.create(Review, {
+        rating: req.body.rating,
+        description: req.body.description,
+        reviewOwner: user.id,
+        showReviewd: newContent,
+        comments: [],
+      });
+    }
+
+    await em.persistAndFlush(newReview);
+
+    const reviewsCount = await em.count(Review, { reviewOwner: user.id });
+    const newRangoCinefilo = await em.findOne(RangoCinefilo, {
+      minReviews: { $lte: reviewsCount },
+    }, { orderBy: { minReviews: 'DESC' } });
+
     let rangoChanged = false;
     if (newRangoCinefilo && user.rangoCinefilo.id !== newRangoCinefilo.id) {
       user.rangoCinefilo = newRangoCinefilo;
@@ -70,10 +64,13 @@ async function addOneReview(req: Request, res: Response) {
       rangoChanged,
       newRango: newRangoCinefilo ? newRangoCinefilo.nameRango : null,
     });
+
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 }
+
+      
 
 async function deleteOneReview(req: Request, res: Response) {
   try {
@@ -110,9 +107,21 @@ async function editReview(req: Request, res: Response) {
     const reviewOwner = Number.parseInt(req.params.id);
     const showReviewd = (await findOneContent(req, res)) as ShowContent;
     const review = await em.findOneOrFail(Review, { reviewOwner, showReviewd });
-    review.rating = req.body.rating;
-    review.description = req.body.description;
-    await em.flush();
+    const toxicityScore = await perspectiveAnalisys.analyzeText(
+      req.body.description
+    );
+    if (toxicityScore == null) {
+      res.status(500).json({ message: "error with perspective api" });
+      return;
+    }
+    if (toxicityScore > 0.7) {
+      res.status(200).json({ message: "Toxicity detected" });
+      return;
+    } else {
+      review.rating = req.body.rating;
+      review.description = req.body.description;
+      await em.flush();
+    }
     res.status(200).json({ message: "review edited", data: review });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
